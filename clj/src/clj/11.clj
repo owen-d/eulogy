ae32c610a13f94ef74bfc1f01aa57d45ae3b4e79
Owen Diehl <ow.diehl@gmail.com>
2016-11-15 17:14:56 -0500

    implements a deep map for preprocessing string-array for # 11

--- clj/src/clj/11.clj
+++ clj/src/clj/11.clj
@@ -12,7 +12,18 @@
         (build-matrix-from-string others %1))
      separated)))
 
+(defn deep-map
+  [mapper type-check nested-col]
+  (mapv
+   #(if-not (= (type %1) type-check)
+      (mapper %1)
+      (deep-map mapper type-check %1))
+   nested-col))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (build-matrix-from-string ["\n" " "] (get-text))))
+  (println (let
+               [split-str (build-matrix-from-string ["\n" " "] (get-text))
+                int-matrix (deep-map #(. Integer parseInt %1) clojure.lang.PersistentVector split-str)]
+             int-matrix)))
