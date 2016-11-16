a7951da5606546ba641d52b3b2b4204143198f1c
Owen Diehl <ow.diehl@gmail.com>
2016-11-16 11:59:34 -0500

    begins traversal fns for # 11

--- clj/src/clj/11.clj
+++ clj/src/clj/11.clj
@@ -20,10 +20,61 @@
       (deep-map mapper type-check %1))
    nested-col))
 
+(defn find-node
+  [[& coords] sub-matrix]
+  (if (or
+       (empty? coords)
+       (not sub-matrix))
+    ;we've descended correctly or have reached a boundary so return node/zero.
+    (or sub-matrix 0)
+    ;otherwise descend
+    (recur (rest coords) (get sub-matrix (first coords)))))
+
+(defn traverse-next-coords
+  [directions coords]
+  (map + directions coords))
+
+(defn get-traverse-right-coords
+  [coords]
+  ((partial traverse-next-coords [0 1]) coords))
+
+(defn get-traverse-down-right-coords
+  [coords]
+  ((partial traverse-next-coords [1 1]) coords))
+
+(defn get-traverse-down-coords
+  [coords]
+  ((partial traverse-next-coords [1 0]) coords))
+
+(defn get-traverse-down-left-coords
+  [coords]
+  ((partial traverse-next-coords [1 -1]) coords))
+
+(defn multiply-nodes
+  "adds consecutive nodes in a given direction in a matrix"
+  ([direction-fn matrix init-node-coords]
+   (multiply-nodes direction-fn matrix init-node-coords 4 0))
+  ([direction-fn matrix init-node-coords count sum]
+   (if (= count 0)
+     ;done, return sum
+     sum
+     (recur direction-fn matrix (direction-fn init-node-coords) (dec count) (+ sum (find-node init-node-coords matrix))))))
+
+(defn get-all-coords
+  ([matrix]
+   (get-all-coords matrix []))
+  ([matrix coords]
+   (if (number? matrix)
+     coords
+     (map-indexed #(get-all-coords %2 (conj coords %1))
+                  matrix))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (let
-               [split-str (build-matrix-from-string ["\n" " "] (get-text))
-                int-matrix (deep-map #(. Integer parseInt %1) clojure.lang.PersistentVector split-str)]
-             int-matrix)))
+  println (let
+              [split-str (build-matrix-from-string ["\n" " "] (get-text))
+               int-matrix (deep-map #(. Integer parseInt %1) clojure.lang.PersistentVector split-str)
+               node-list (reduce #(concat %1 %2) (get-all-coords int-matrix))]
+            node-list))
+;(multiply-nodes get-traverse-down-coords int-matrix [0 0])
