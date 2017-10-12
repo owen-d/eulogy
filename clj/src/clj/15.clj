1eb1946b0f5295a242511db1c95c7cb636bf4a53
owen-d <ow.diehl@gmail.com>
2017-10-11 21:21:04 -0400

    memoizes lattice-paths

--- clj/src/clj/15.clj
+++ clj/src/clj/15.clj
@@ -5,11 +5,18 @@
 (defn lattice-paths
   ""
   ([n]
-   (lattice-paths n 0 0))
-  ([n x y]
-   (cond (or (> x n) (> y n)) 0
-         (and (= x n) (= y n)) 1
-         :else (+ (lattice-paths n (+ x 1) y)
-                  (lattice-paths n x (+ y 1))))))
+   (lattice-paths n 0 0 {}))
+  ([n x y store]
+   (let [found (get store [x y])]
+     (cond (or (> x n) (> y n)) [store 0]
+           (and (= x n) (= y n)) [store 1]
+           ;; if we already have a memoized # of subpaths from here, use it!
+           found [store found]
+           :else
+           ;; otherwise, branch
+           (let [[first-store first-sum] (lattice-paths n (+ x 1) y store)
+                 [second-store second-sum] (lattice-paths n x (+ y 1) first-store)]
+             [(assoc second-store [x y] (+ first-sum second-sum)) (+ first-sum second-sum)])))))
 
-(lattice-paths 20)
+
+(second (lattice-paths 20))
